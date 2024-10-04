abae2070797ccc5a5aed36ae6920088a8e84d30d
Soroushsrd <sardashtisoroosh@gmail.com>
2024-10-04 21:22:52 +0330

    Encode torrent 'pieces' as base64 in JSON decoder
    Modified the `decode_json` function to encode the 'pieces' field as base64 when converting the torrent file to JSON. This ensures compatibility and proper handling of binary data within the JSON output.

--- src-tauri/src/parser.rs
+++ src-tauri/src/parser.rs
@@ -1,16 +1,20 @@
-use bencode_encoder::{Decoder, Encoder};
+use bencode_encoder::{Decoder, Encoder, Type};
+use bencode_encoder::Type::Dictionary;
 use std::path::Path;
-use std::fs::File;
+use std::fs::{File};
 use std::io::Write;
 use sha1::{Digest, Sha1};
+use base64::{Engine as _, engine::{self, general_purpose}, alphabet};
 
+const CUSTOM_ENGINE: engine::GeneralPurpose =
+    engine::GeneralPurpose::new(&alphabet::URL_SAFE, general_purpose::NO_PAD);
 pub fn calculate_info_hash(torrent_path: &str) -> Result<[u8; 20], Box<dyn std::error::Error>> {
     let path = Path::new(torrent_path);
     if !path.exists(){
         return Err(format!("Input file does not exist: {}", torrent_path).into());
     }
 
-    let decoded = Decoder::decode_from(path)?;
+    let decoded = Decoder::decode_from(path).unwrap();
     let info = decoded.get("info".to_string()).expect("Could not find the info part");
     let info_bencoded = Encoder::encode(info).expect("Could not bencode the info values");
     let mut hasher = Sha1::new();
@@ -18,9 +22,9 @@ pub fn calculate_info_hash(torrent_path: &str) -> Result<[u8; 20], Box<dyn std::
     hasher.update(info_bencoded);
     Ok(hasher.finalize().into())
 
-
-
 }
+
+// fix the pieces part!
 pub fn decode_json(bpath: &str, opath: &str) -> Result<(), Box<dyn std::error::Error>> {
     let input_path = Path::new(bpath);
     let output_path = Path::new(opath);
@@ -31,9 +35,21 @@ pub fn decode_json(bpath: &str, opath: &str) -> Result<(), Box<dyn std::error::E
 
     match Decoder::decode_from(bpath) {
         Err(e) => Err(format!("Error opening or decoding file: {}", e).into()),
-        Ok(t) => {
+        Ok(mut t) => {
             println!("Successfully decoded torrent file");
-            let json_content = t.to_json()?;
+
+            if let Dictionary(ref mut dict) = t{
+                if let Some(pieces) = dict.get_mut("pieces"){
+
+                    if let Type::ByteString(ref pieces_bytes) = pieces{
+                        let mut pieces_base64 = String::new();
+                        general_purpose::STANDARD.encode_string(pieces_bytes.as_bytes(),&mut pieces_base64);
+                        *pieces = Type::ByteString(pieces_base64);
+                    }
+                }
+            }
+
+            let json_content = t.to_json().unwrap();
             match File::create(output_path) {
                 Ok(mut file) => {
                     match file.write_all(json_content.as_bytes()) {
@@ -56,6 +72,6 @@ mod tests {
 
     #[test]
     fn test_decode_json(){
-        decode_json("C:\\Users\\Lenovo\\Downloads\\DreadHaunt [FitGirl Repack].torrent","C:\\Users\\Lenovo\\Downloads\\example.json").unwrap();
+        decode_json("C:\\Users\\Lenovo\\Downloads\\Anomalous [FitGirl Repack].torrent","C:\\Users\\Lenovo\\Downloads\\Anomalous [FitGirl Repack].json").unwrap();
     }
 }
