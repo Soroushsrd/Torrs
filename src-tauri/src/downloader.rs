78ba7a37d1c358997c28911fdae75d7e2fbe52c9
Soroushsrd <sardashtisoroosh@gmail.com>
2024-10-09 10:37:59 +0330

    Refactor Peer and Peer info into download functions

--- src-tauri/src/downloader.rs
+++ src-tauri/src/downloader.rs
@@ -4,14 +4,15 @@ use rand::Rng;
 use reqwest;
 use serde::Deserialize;
 use serde_bencode;
+use crate::peer::{Peer, PeerInfo};
 
 #[derive(Debug,Deserialize)]
 pub struct TrackerResponse{
     interval:i64,
-    peer: Vec<u8>
+    peer: Vec<PeerInfo>
 }
 
-
+/// Used to generate Peer Id that is later used to request trackers
 pub fn generate_peer_id() ->[u8;20]{
     let mut rng = rand::thread_rng();
     let mut peer_id =[0u8;20];
@@ -24,7 +25,8 @@ pub fn generate_peer_id() ->[u8;20]{
     peer_id
 }
 
-pub async fn request_peers(torrent: &TorrentMetaData)->Result<Vec<(String,u16)>, Box<dyn std::error::Error>>{
+/// Request Peers in order to get the Peer Info that is needed to establish connections.
+pub async fn request_peers(torrent: &TorrentMetaData)->Result<Vec<PeerInfo>, Box<dyn std::error::Error>>{
     let info_hash = torrent.calculate_info_hash()?;
     let trackers = torrent.get_tracker_url();
     let total_length = torrent.get_total_size();
@@ -38,8 +40,9 @@ pub async fn request_peers(torrent: &TorrentMetaData)->Result<Vec<(String,u16)>,
 
 }
 
+/// Request Trackers based on the info that has been parsed from torrent file.
 pub async fn request_tracker(announce: &str, info_hash:&[u8;20], total_length:i64)
-    ->Result<Vec<(String,u16)>, Box<dyn std::error::Error>> {
+    ->Result<Vec<PeerInfo>, Box<dyn std::error::Error>> {
     let mut url = Url::parse(announce)?;
     let peer_id = generate_peer_id();
 
@@ -56,19 +59,12 @@ pub async fn request_tracker(announce: &str, info_hash:&[u8;20], total_length:i6
     let response = reqwest::get(url).await.unwrap().bytes().await?;
     let tracker_response: TrackerResponse = serde_bencode::from_bytes(&response)?;
 
-    Ok(parse_peers(&tracker_response.peer))
+    Ok(tracker_response.peer)
 }
 
-fn parse_peers(peers:&[u8])->Vec<(String,u16)>{
-    peers.chunks(6)
-        .map(|chunk| {
-            let ip = chunk[..4].iter().map(|&b| b.to_string()).collect::<Vec<String>>().join(".");
-            let port = u16::from_be_bytes([chunk[4],chunk[5]]);
-            (ip,port)
-        })
-        .collect()
-}
 
+
+/// Encodes url to a String
 fn urlencode(bytes:&[u8])->String{
     bytes.iter()
         .map(|&b| format!("%{:02X}", b))
