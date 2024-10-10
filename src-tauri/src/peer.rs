bcd6d6cc846af5f53553a6f0859ad4e4936ba0a7
Soroushsrd <sardashtisoroosh@gmail.com>
2024-10-10 21:22:23 +0330

    Refactor peer piece handling and file operations
    Revised the `request_piece` function to manage piece downloading, including handling bitfield and unchoke messages. Additionally, the function now writes data directly to a file, enhancing efficiency and clarity.

--- src-tauri/src/peer.rs
+++ src-tauri/src/peer.rs
@@ -2,7 +2,7 @@ use tokio::net::TcpStream;
 use std::error::Error;
 use serde::{Deserialize, Serialize};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use url::quirks::protocol;
+use tokio::fs::File;
 
 #[derive(Debug, Deserialize,Serialize,Clone)]
 pub struct PeerInfo{
@@ -67,35 +67,90 @@ impl Peer {
         Ok(())
     }
 
-    pub async fn request_piece(&mut self, index: u32, begin: u32, length: u32) -> Result<Vec<u8>, Box<dyn Error>> {
+    pub async fn request_piece(&mut self, index: u32, piece_length: u32, file_path: &str) -> Result<(), Box<dyn Error>> {
         if self.stream.is_none() {
             return Err("Not connected to peer".into());
         }
 
         let stream = self.stream.as_mut().unwrap();
-
-        let mut request :Vec<u8> = Vec::new();
-        request.extend_from_slice(&(13u32).to_be_bytes());
-        request.push(6);
-        request.extend_from_slice(&index.to_be_bytes());
-        request.extend_from_slice(&begin.to_be_bytes());
-        request.extend_from_slice(&length.to_be_bytes());
-
-        stream.write_all(&request).await?;
-
+        //////////////////////////// Waiting for bitfield //////////////////////////////
         let mut msg_len = [0u8;4];
         stream.read_exact(&mut msg_len).await?;
         let msg_len = u32::from_be_bytes(msg_len) as usize;
 
         let mut msg_id = [0u8;1];
         stream.read_exact(&mut msg_id).await?;
-
-        if msg_id[0] != 7 { // 7 is the message id for piece
-            return Err("Invalid msg Id!".into())
+        let msg_id = u32::from_be_bytes(msg_id) as usize;
+        if msg_id[0] != 5 {
+            return Err("Expected bitfield message".into());
         }
-        let mut piece_data = vec![0u8; msg_len-9];
-        stream.read_exact(&mut piece_data).await?;
 
-        Ok(piece_data)
+        let mut bitfield_payload = vec![0u8;msg_len-1];
+        stream.read_exact(&mut bitfield_payload).await?;
+
+        //////////////////////////// Send interested message //////////////////////////////
+        let interested_msg = [0u8,0,0,1,2];
+        stream.write_all(&interested_msg).await?;
+
+        //////////////////////////// Wait for unchoke message //////////////////////////////
+
+        let mut msg_len =[0u8;4];
+        stream.read_exact(&mut msg_len).await?;
+        let msg_len = u32::from_be_bytes(msg_len) as usize;
+
+        let mut msg_id = [0u8;1];
+        stream.read_exact(&mut msg_id).await?;
+        let msg_id = u32::from_be_bytes(msg_id) as usize;
+        if msg_id[0] != 1 {
+            return Err("Expected unchoke message".into());
+        }
+
+
+        //////////////////////////// Requesting Piece //////////////////////////////
+        let mut file = File::create(file_path).await?;
+        let block_size = 16*1024;
+        let mut offset = 0;
+
+        while offset<piece_length{
+            let block_length = if piece_length-offset < block_size{
+                piece_length-offset
+            }else{
+                block_size
+            };
+            let mut request :Vec<u8> = Vec::new();
+            request.extend_from_slice(&(13u32).to_be_bytes());
+            request.push(6);
+            request.extend_from_slice(&index.to_be_bytes());
+            request.extend_from_slice(&offset.to_be_bytes());
+            request.extend_from_slice(&block_length.to_be_bytes());
+            stream.write_all(&request).await?;
+
+            let mut msg_len = [0u8;4];
+            stream.read_exact(&mut msg_len).await?;
+            let msg_len = u32::from_be_bytes(msg_len) as usize;
+
+            let mut msg_id = [0u8;1];
+            stream.read_exact(&mut msg_id).await?;
+
+            if msg_id[0] != 7 {
+                return Err("Invalid msg Id!".into())
+            }
+
+            let mut piece_index = [0u8; 4];
+            stream.read_exact(&mut piece_index).await?;
+            let mut piece_offset = [0u8; 4];
+            stream.read_exact(&mut piece_offset).await?;
+
+            let mut block = vec![0u8; msg_len - 9];
+            stream.read_exact(&mut block).await?;
+
+            file.write_all(&block).await?;
+
+            offset += block_length;
+
+        }
+
+        Ok(())
     }
 }
+
